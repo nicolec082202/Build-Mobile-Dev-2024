f4ec6359e3da131c155f1aec75c702d77fed3109
NicolePotter <ncruz071@fiu.edu>
2024-10-06 21:36:34 -0400

    Initial Commit

---
+++ MobileDev_NiceBabies/MobileDev_NiceBabiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileDev_NiceBabiesApp.swift
+//  MobileDev_NiceBabies
+//
+//  Created by Nicole Potter on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileDev_NiceBabiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
